8c0303e1be8b026d721be667157123722ffce727
Jawaid Aziz <135430447+javaid24@users.noreply.github.com>
2024-06-23 13:30:14 +0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+const About = () => {
+  return (
+    <div>
+    <h1>About Page</h1>
+  </div>
+  );
+};
+
+const User = () => {
+  const params = useParams();
+  console.log("params", params);
+  return (
+    <div>
+      <h1>User name: </h1>
+    </div>
+  );
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/user/:userid" element={<User/>}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
