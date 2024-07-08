9caaaaf7d098a72070d883a678279f2b32f71d94
Jawaid Aziz <javaidmemon24@gmail.com>
2024-07-08 23:57:41 +0500

    commit

---
+++ src/Components/Text.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Text = () => {
+    return (
+        <h1>I am a Text.</h1>
+    );
+};
--- src/Pages/Edit.jsx
+++ src/Pages/Edit.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { Text } from "../Components/Text";
 
 export const EditPage = () => {
 
     const [params] = useSearchParams();
-
+    const [count, setCount] = useState(0);
+    const addText = () => {
+        setCount(count + 1);
+    }
 return (
-    
     <div>
-        <img src={params.get("url")} width="400px"/>
+        <div>
+            <img src={params.get("url")} width="400px"/>
+            {
+                Array(count).fill(0).map(e => <Text/>)
+            }
+        </div>
+        <Button onClick={addText()}>Add Text</Button>
     </div>
 )
 };
