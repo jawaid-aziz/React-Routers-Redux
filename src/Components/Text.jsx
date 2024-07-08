b5589ad58388ae572ece8f4fc6f7dcb5a0199649
Jawaid Aziz <javaidmemon24@gmail.com>
2024-07-09 00:31:52 +0500

    commit

--- src/Components/Text.jsx
+++ src/Components/Text.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import Draggable from "react-draggable";
 
 export const Text = () => {
+    const [editMode, setEditMode] = useState(false);
+    const [val, setVal] = useState("Double Click to Edit");
+
+
     return (
-        <h1>I am a Text.</h1>
+        <Draggable>
+            {
+                editMode ? ( <input onDoubleClick={e => setEditMode(false)} value={val} onChange={(e) => setVal(e.target.value)} />
+            ) : (
+                <h1 onDoubleClick={e=> setEditMode(true)} >{val}</h1>
+            )} 
+        </Draggable>
     );
 };
