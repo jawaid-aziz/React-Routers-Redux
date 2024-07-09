82b70dede1626755482fa46c15c690e57b21bfe6
Jawaid Aziz <javaidmemon24@gmail.com>
2024-07-09 20:30:51 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import { Counter } from './Components/Count';
-import { useDispatch } from 'react-redux';
 
 function App() {
   const dispatch = useDispatch();
   return (
     <div className="container">
-      <button onClick={e=> dispatch({type:'INCREMENT'})}>Increment</button>
-      <Counter/>
-      <button onClick={e=> dispatch({type:'DECREMENT'})}>Decrement</button>
+
     </div>
   );
 };
