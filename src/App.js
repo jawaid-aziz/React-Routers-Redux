14b53660875ad9e1ed823d538e2ac3ec4990ed47
Jawaid Aziz <javaidmemon24@gmail.com>
2024-07-09 21:02:27 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { fetchTodos } from './redux/slice/todo';
 
 function App() {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+
+  if (state.todo.isLoading) {
+    return <h1>Loading....</h1>
+  }
+
   return (
     <div className="container">
-      
+      <button onClick={(e) => dispatch(fetchTodos())}>Fetch Todos'</button>
+      {state.todo.data && state.todo.data.map( (e) => <li>{e.title}</li>)}
     </div>
   );
 };
